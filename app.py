e7c40943acb5a0a3da1c1832e0092750eb964883
Krithika <krithika.ragha@gmail.com>
2019-05-21 19:21:16 -0400

    Added updated intro.html and style.css

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 from flask import Flask, request, render_template, flash, redirect, url_for, session, logging
-from flask_sqlalchemy import SQLAlchemy
 import os
 
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
+    return render_template('intro.html')
+
+@app.route('/home')
+def home():
     return render_template('home.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
-
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')

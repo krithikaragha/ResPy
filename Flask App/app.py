7bbfa161c482e199251a82682bed1f9bb2ef5b05
Krithika <krithika.ragha@gmail.com>
2019-05-22 14:22:34 -0400

    Restructuring: Updated about.html, maps.html and combined navbar.html with layout.html

---
+++ Flask App/app.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask, request, render_template, flash, redirect, url_for, session, logging
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/maps')
+def maps():
+    return render_template('maps.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
